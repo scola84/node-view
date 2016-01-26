c1a9e7dabaed256ecc6887bcd2c7753fdd8832e7
M.C. van den Boogaart <scola@kvdb.name>
2016-01-26 12:11:30 +0100

    Add model

--- index.js
+++ index.js
@@ -5,11 +5,13 @@ const DI = require('@scola/di');
 const Abstract = require('./lib/abstract');
 const Dispatcher = require('./lib/dispatcher');
 const Element = require('./lib/element');
+const Model = require('./lib/model');
 
 class Module extends DI.Module {
   configure() {
     this.inject(Dispatcher).with(
-      this.provider(Element)
+      this.provider(Element),
+      this.provider(Model)
     );
 
     if (window) {
@@ -24,5 +26,6 @@ module.exports = {
   Abstract,
   Dispatcher,
   Element,
+  Model,
   Module
 };
